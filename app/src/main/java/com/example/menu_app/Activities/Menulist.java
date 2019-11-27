d6408447fbfb0a9c0d8489210942da15f0ba414f
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-11-27 20:37:51 +0200

    I added the searchView

--- app/src/main/java/com/example/menu_app/Activities/Menulist.java
+++ app/src/main/java/com/example/menu_app/Activities/Menulist.java
@@ -1,102 +1,191 @@
 package com.example.menu_app.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.menu_app.R;
-import com.example.menu_app.adapter.menulistAdapter;
+import com.example.menu_app.adapter.Myadapter;
+import com.example.menu_app.models.Model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Menulist extends AppCompatActivity {
-    private static final String TAG = "Menulist";
-
-    private ArrayList<String> Names=new ArrayList<>();
-    private ArrayList<String> Imageurls=new ArrayList<>();
-    private ArrayList<String> Money=new ArrayList<>();
+    RecyclerView recyclerView;
+    Myadapter myadapter;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menulist);
-        Log.d(TAG, "onCreate: started");
-        RecyclerView recyclerView=findViewById(R.id.recyclerview);
-        menulistAdapter adapter=new menulistAdapter(this,Names,Imageurls,Money);
-        recyclerView.setAdapter(adapter);
+        recyclerView=findViewById(R.id.recyclerView);
+        preferences=this.getSharedPreferences("Preference",MODE_PRIVATE);
+        getMyList();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater =getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        MenuItem item=menu.findItem(R.id.search);
+        SearchView searchView=(SearchView) MenuItemCompat.getActionView(item);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                myadapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                myadapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if (id==R.id.sorting){
+            sortDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sortDialog(){
+        String[] options={"Ascending","Descending"};
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+
+        builder.setTitle("Sort by");
+        builder.setIcon(R.drawable.ic_sort_black_24dp);
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which==0){
+                    SharedPreferences.Editor editor=preferences.edit();
+                    editor.putString("Sort","ascending");
+                    editor.apply();
+                    getMyList();
+                }
+                if (which==1){
+                    SharedPreferences.Editor editor=preferences.edit();
+                    editor.putString("Sort","descending");
+                    editor.apply();
+                    getMyList();
+                }
+            }
+        });
+
+        builder.create().show();
+    }
+
+
+
+    private void  getMyList(){
+        ArrayList<Model>models=new ArrayList<>();
+
+        Model model= new Model();
+        model.setName("Apple Pie");
+        model.setImage(R.drawable.food5);
+        model.setPrice("5600");
+        models.add(model);
+
+        model= new Model();
+        model.setName("Clam Chowder");
+        model.setImage(R.drawable.food);
+        model.setPrice("4500");
+        models.add(model);
+
+        model= new Model();
+        model.setName("Bagel and Lox");
+        model.setImage(R.drawable.background);
+        model.setPrice("2500");
+        models.add(model);
+
+
+        model= new Model();
+        model.setName("Deep-Dish Pizza");
+        model.setImage(R.drawable.chivas);
+        model.setPrice("1500");
+        models.add(model);
+
+        model= new Model();
+        model.setName("Sausage Gravy");
+        model.setImage(R.drawable.d1);
+        model.setPrice("6000");
+        models.add(model);
+
+        model= new Model();
+        model.setName("Texas Barbecue");
+        model.setImage(R.drawable.d2);
+        model.setPrice("3000");
+        models.add(model);
+
+        model= new Model();
+        model.setName("Water melon");
+        model.setImage(R.drawable.d4);
+        model.setPrice("9000");
+        models.add(model);
+
+        model= new Model();
+        model.setName("snifter");
+        model.setImage(R.drawable.d3);
+        model.setPrice("1000");
+        models.add(model);
+
+        model= new Model();
+        model.setName("Liquor");
+        model.setImage(R.drawable.drinks);
+        model.setPrice("2000");
+        models.add(model);
+
+        model= new Model();
+        model.setName("Chicken");
+        model.setImage(R.drawable.food3);
+        model.setPrice("7000");
+        models.add(model);
+
+        model= new Model();
+        model.setName("Fried Chicken");
+        model.setImage(R.drawable.food2);
+        model.setPrice("4000");
+        models.add(model);
+
+        String sortSetting =preferences.getString("Sort","ascending");
+
+        if (sortSetting.equals("ascending")){
+            Collections.sort(models,Model.By_Menu_Ascending);
+        }else if (sortSetting.equals("descending")){
+            Collections.sort(models,Model.By_Menu_Descending);
+        }
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ImageBitmaps();
+        myadapter=new Myadapter(this,models);
+        recyclerView.setAdapter(myadapter);
+
+
 
     }
-    private void  ImageBitmaps(){
-        Log.d(TAG, "initImageBitmaps: preparing images");
 
-        Imageurls.add("https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add(" Apple Pie");
-        Money.add("2000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=653&q=80");
-        Names.add("Clam Chowder");
-        Money.add("3000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
-        Names.add(" Bagel and Lox");
-        Money.add("9000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("Deep-Dish Pizza");
-        Money.add("6000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add(" Sausage Gravy");
-        Money.add("4000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1547595628-c61a29f496f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("Texas Barbecue");
-        Money.add("7000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1513558161293-cdaf765ed2fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("Water melon");
-        Money.add("3000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1438557068880-c5f474830377?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("snifter");
-        Money.add("2000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1545438102-799c3991ffb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("cocktail");
-        Money.add("1000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1470337458703-46ad1756a187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("Liquor");
-        Money.add("9000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1532550907401-a500c9a57435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("Chicken");
-        Money.add("1000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1516684669134-de6f7c473a2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("Fried Chicken");
-        Money.add("7000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("Pizza");
-        Money.add("9000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1520201163981-8cc95007dd2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("Pizza cheese");
-        Money.add("2000");
-
-        Imageurls.add("https://images.unsplash.com/photo-1549611016-3a70d82b5040?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("The Hamburger");
-        Money.add("1500");
-
-        Imageurls.add("https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
-        Names.add("fried meat");
-        Money.add("5000");
-
-    }
 }
--- app/src/main/java/com/example/menu_app/constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.menu_app;
-
-public class constants {
-
-    public static final String preference_tablenbr = "TableNbr";
-}
