23cf06075a3987626776a2e041bda845b1e9f8ad
Omoyi <urakhel7@gmail.com>
2019-12-05 17:23:19 +0200

    initial commit

---
+++ app/src/main/java/com/example/menu_app/Activities/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.menu_app.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.menu_app.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.btnWaiter)Button waiter;
+    @BindView(R.id.btnKitchen)Button kitchen;
+    @BindView(R.id.btnReceipts)Button receipts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        waiter.setOnClickListener(this);
+        kitchen.setOnClickListener(this);
+        receipts.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == waiter) {
+            Intent waiter = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(waiter);
+        }
+
+        if(v == kitchen) {
+            Intent kitchen = new Intent(MainActivity.this, QuantityActivity.class);
+            startActivity(kitchen);
+        }
+
+        if(v == receipts) {
+            Intent receipts = new Intent(MainActivity.this, Tablenbr.class);
+            startActivity(receipts);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/menu_app/ItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.menu_app;
+
+import android.view.View;
+
+public interface ItemClickListener {
+
+    void onItemClickListener(View view, int position);
+}
